20fbc0e2af872482220e0ae4a786c69673a7cf6a
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-05-12 21:43:59 +0530

    Separate the main header background to a different component

---
+++ src/components/main-header/main-header.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './main-header.module.css';
+
+import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from './main-header-background';
+
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+
+      <header className={styles.header}>
+        <Link className={styles.logo} href='/'>
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>{' '}
+            <li>
+              <Link href='/community'>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
