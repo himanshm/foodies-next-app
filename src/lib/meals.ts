6c6774e1f77dd46dce58734c9dd15f4728bfa67f
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-05-19 14:32:57 +0530

    Creating a Slug & Sanitizing User Input for XSS Protection

--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -1,4 +1,16 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+
+interface Meal {
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+}
 
 const db = sql('meals.db');
 
@@ -9,4 +21,9 @@ export async function getMeals() {
 
 export function getMeal(slug: string) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export function saveMeal(meal: Meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
