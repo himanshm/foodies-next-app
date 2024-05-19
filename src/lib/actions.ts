4b1a2e5f9db3f56de112096b73d70ba1fee0a640
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-05-19 16:52:00 +0530

    Store uploaded images and store data in database

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,14 +1,29 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { saveMeal } from './meals';
+
 export async function shareMeal(formData: FormData) {
   const meal = {
-    title: formData.get('title'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    image: formData.get('image'),
-    creator: formData.get('name'),
-    creator_email: formData.get('email'),
+    title: formData.get('title') as string,
+    summary: formData.get('summary') as string,
+    instructions: formData.get('instructions') as string,
+    image: formData.get('image') as File,
+    creator: formData.get('name') as string,
+    creator_email: formData.get('email') as string,
   };
 
-  console.log(meal);
+  if (
+    !meal.title ||
+    !meal.summary ||
+    !meal.instructions ||
+    !meal.image ||
+    !meal.creator ||
+    !meal.creator_email
+  ) {
+    throw new Error('Missing required fields');
+  }
+
+  await saveMeal(meal);
+  redirect('/meals');
 }
--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -1,11 +1,13 @@
+import fs from 'node:fs';
+
 import sql from 'better-sqlite3';
 import slugify from 'slugify';
 import xss from 'xss';
 
 interface Meal {
   title: string;
-  slug: string;
-  image: string;
+  slug?: string;
+  image: File;
   summary: string;
   instructions: string;
   creator: string;
@@ -23,7 +25,45 @@ export function getMeal(slug: string) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
 
-export function saveMeal(meal: Meal) {
+export async function saveMeal(meal: Meal) {
   meal.slug = slugify(meal.title, { lower: true });
   meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  // const extension = path.extname(meal.image.name);
+  const timestamp = Date.now().toString(36);
+  const randomString = Math.random().toString(36).substring(2, 6);
+  const guid = `${timestamp}${randomString}`.substring(0, 10);
+  const fileName = `${meal.slug}-${guid}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+
+  // Convert File to arrayBuffer and then to Buffer
+  const bufferedImage = await meal.image.arrayBuffer();
+  const buffer = Buffer.from(bufferedImage);
+
+  stream.write(buffer, (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+
+  const imageFileName = `/images/${fileName}`;
+
+  const stmt = db.prepare(`
+  INSERT INTO meals
+  (title, summary, instructions, creator, creator_email, image, slug)
+  VALUES 
+  (@title, @summary, @instructions, @creator, @creator_email, @image, @slug)
+  `);
+
+  stmt.run({
+    title: meal.title,
+    summary: meal.summary,
+    instructions: meal.instructions,
+    creator: meal.creator,
+    creator_email: meal.creator_email,
+    image: imageFileName,
+    slug: meal.slug,
+  });
 }
